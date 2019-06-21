4b3e1909241b6e7549790ee06d77a059252c739e
Dmitry Boyko <dmitry@thebodva.com>
2019-06-21 22:22:54 +0300

    add highload action to test the time

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {write, Writer} from './io/write'
 import {Matrix, WRAPPED} from "./model/model.js";
 import {Solution} from "./model/solution.js";
 import Solver from "./solve";
-import {parseState} from "./model/model";
+import {Coord, FREE, OBSTACLE, parseState, State} from "./model/model";
 
 const exec = (model: string, callback: Function) => {
 
@@ -24,8 +24,34 @@ const exec = (model: string, callback: Function) => {
     callback();
 };
 
+const highload = () => {
+    let start = new Date();
+    let s = new State(400, 400);
+    s.workerPos = new Coord(1, 1);
+    for (let j = 0; j < 400; j++) {
+        for (let i = 0; i < 400; i++) {
+            if (Math.random() < 0.95 || (i === 1 && j === 1)) {
+                s.m.set(i, j, FREE);
+            } else {
+                s.m.set(i,j, OBSTACLE)
+            }
+        }
+    }
+    console.log(s.dump());
+
+    let solver = new Solver(s);
+    let solution = solver.solve();
+    let writer = new Writer(solution);
+
+    writer.write('solutions', '001');
+    let end = new Date() - start;
+    console.info('Execution time: %dms', end)
+
+};
+
 if (process.send === undefined) {
-    exec('001',() => 0);
+    exec('001', () => 0);
+    // highload()
 }
 
 module.exports = {
