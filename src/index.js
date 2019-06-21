4f1b9ad871033c3e2213d18cee6f4cc3c4babeb1
Dmitry Boyko <dmitry@thebodva.com>
2019-06-21 17:37:19 +0300

    update launcher for multiple cores

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import {write} from './io/write'
 import {Matrix, WRAPPED} from "./model/model.js";
 import Solver from "./solve";
 
-const exec = () => {
+const exec = (callback: Function) => {
     console.log(2);
     let m = new Matrix(15, 6);
     m.set(0, 0, WRAPPED);
     console.log(2.5);
     console.log(m.dump());
     console.log(3);
+
+    callback();
 };
 
 if (process.send === undefined) {
-
-    exec();
+    exec(() => 0);
 }
-
 
 module.exports = {
     exec
