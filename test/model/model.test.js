ea0383e5abefa7a00504e6362617f8cf13647418
Andrei Tsygankov <andrei.tsygankov@gmail.com>
2019-06-21 16:12:51 +0300

    + dump test

--- test/model/model.test.js
+++ test/model/model.test.js
@@ -1,7 +1,19 @@
 import {  } from "../../src/model/model";
 import assert from 'assert';
+import { Matrix } from "../../src/model/model";
+import { WRAPPED, OBSTACLE } from "../../src/model/model";
 
-describe("model test", () => {
-  test("model", () => {
+describe("Basic model", () => {
+  test("Matrix dump", () => {
+    const empty =
+      `|  |\n|  |\n`;
+    let m = new Matrix(2, 2);
+    expect(m.dump()).toEqual(empty);
+
+    m.set(0, 0, WRAPPED);
+    m.set(1, 0, OBSTACLE);
+    const one =
+      `|  |\n|.#|\n`;
+    expect(m.dump()).toEqual(one);
   })
-})
+});
