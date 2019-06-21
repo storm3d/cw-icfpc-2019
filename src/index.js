d022f5b7b513c78f82b2ae6b90103f9e975d84de
Dmitry Boyko <dmitry@thebodva.com>
2019-06-21 15:42:09 +0300

    launcher

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 // import {write} from './io/write'
 // import Solver from "./solve";
 
+import {formatNum} from "./runner/launch";
+
 const exec = (inputFolder: string, outputFolder: string, num: string) => {
     // eslint-disable-next-line no-console
     console.log(inputFolder, outputFolder, num);
@@ -15,22 +17,27 @@ const exec = (inputFolder: string, outputFolder: string, num: string) => {
 };
 
 if (process.send === undefined) {
-    exec('part-1-initial', "solutions", "001");
+    let models = Array.from({length: 150}, (v, k) => k + 1);
+    while (models.length > 0) {
+        let model = models.pop();
+        let modelname = formatNum(model, 3);
+        // exec('part-1-initial', "solutions", modelname);
+    }
 }
 
 // msg it is the number of the task
 process.on('message', (msg) => {
-    // eslint-disable-next-line no-console
-    console.log(msg);
-     if (msg === 'kill') {
-         process.exit(0);
-     } else if (msg === 'ask') {
-         if (process.send !== undefined) {
-             process.send('message');
-         }
-     } else {
-         exec('part-1-initial', 'solutions', msg);
-     }
+//     // eslint-disable-next-line no-console
+//     console.log(msg);
+//     if (msg === 'kill') {
+//         process.exit(0);
+//     } else if (msg === 'ask') {
+        if (process.send !== undefined) {
+            process.send('message');
+        }
+//     } else {
+//         exec('part-1-initial', 'solutions', msg);
+//     }
 });
 
 module.exports = {
