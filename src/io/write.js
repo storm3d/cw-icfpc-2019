5264e0bbcfdf979eefd7474ec9a30491b3e4e62a
Dmitry Boyko <dmitry@thebodva.com>
2019-06-22 16:42:17 +0300

    save the score for every problem

--- src/io/write.js
+++ src/io/write.js
@@ -11,14 +11,44 @@ export class Writer {
 
     write(folder: string, model: string): number {
         let filename = `./${folder}/prob-${model}.sol`;
+        let scorefile = `./scores/prob-${model}.sol`;
+
+        if (!fs.existsSync(scorefile)) {
+            this.writeScore(scorefile);
+
+            return this.writeSolve(filename);
+        }
+
+        let savedScore = parseInt(fs.readFileSync(scorefile).toString(), 10);
+
+        if (savedScore <= this.solution.getScore()) {
+            this.writeScore(scorefile);
+
+            return this.writeSolve(filename);
+        }
+        // eslint-disable-next-line no-console,no-console
+        console.log('New solution get the worst results!');
+        // eslint-disable-next-line no-console
+        console.log(`SOLUTION ${model} WAS NOT UPDATED`);
+
+        return this.solution.getString().length;
+    }
+
+    writeSolve(filename: string): number {
         // eslint-disable-next-line no-console
         console.log(`Writing file ${filename}`);
         fs.writeFileSync(filename, this.solution.getString(), 'utf8');
-        let scoreStr = `${model}:${this.solution.getScore()};`;
-        fs.appendFileSync('./current_result.txt', scoreStr)
         // eslint-disable-next-line no-console
         console.log(`The file ${filename} has been saved!`);
 
         return this.solution.getString().length;
+    }
+
+    writeScore(filename: string): number {
+        fs.writeFileSync(filename, `${this.solution.getScore()}`);
+        // eslint-disable-next-line no-console
+        console.log(`The score file ${filename} has been updated!`);
+
+        return this.solution.getScore();
     }
 }
